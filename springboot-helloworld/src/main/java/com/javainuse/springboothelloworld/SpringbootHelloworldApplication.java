b988b54382c764ea29abde29903e5d623e7c10c4
Bhavya <bhavyamishrasps@gmail.com>
2021-12-09 17:33:02 +0530

    initial commit

---
+++ springboot-helloworld/src/main/java/com/javainuse/springboothelloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.springboothelloworld;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+	public static void main(String[] args) {
+		
+			System.out.println( "Hello World!" );
+		}
+}
+
